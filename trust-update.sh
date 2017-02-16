2f4610b4ec2f0e7fd4fa231ce117162d6de31138
Syaifuddin JW <ucokkarnadi@users.noreply.github.com>
2017-02-16 15:28:24 +0700

    Update trust-update.sh

--- trust-update.sh
+++ trust-update.sh
@@ -7,19 +7,22 @@
 #
 #\$INCLUDE \"/etc/bind/trust.zone\"" > /etc/bind/trust.block
 
-if curl -s --head  --request GET http://trustpositif.kominfo.go.id/ | grep "200 OK" > /dev/null;
-then
-
 SOAF="/etc/bind/trust.block"
 ZONEF="/etc/bind/trust.zone"
+DATE=$(date +%Y%m%d)
+curr=$(sed -n '/^@/,/^[^;]*)/H;${;x;s/.*@[^(]*([^0-9]*//;s/[^0-9].*//;p;}' $SOAF)
+prefix=${curr::-2}
+
+####  Check situs trust positif hidup nggak
+if curl -s --head  --request GET http://trustpositif.kominfo.go.id/ | grep "200 OK" > /dev/null; 
+then
 curl -q -s "http://trustpositif.kominfo.go.id/files/downloads/index.php?dir=database%2Fblacklist%2Fporn%2F&download=domains" | tr '[:upper:]' '[:lower:]' | sed '/\(\/\|:\|=\|\.\.\|\.$\|[0-9]\{1,3\}.[0-9]\{1,3\}.[0-9]\{1,3\}.[0-9]\{1,3\}$\|^$\)/d' | perl -nle 'print if m{^[[:ascii:]]+$}' > /tmp/domain.txt; 
 curl -q -s "http://trustpositif.kominfo.go.id/files/downloads/index.php?dir=database%2Fblacklist%2Fkajian%2F&download=domains" | tr '[:upper:]' '[:lower:]' | sed '/\(\/\|:\|=\|\.\.\|\.$\|[0-9]\{1,3\}.[0-9]\{1,3\}.[0-9]\{1,3\}.[0-9]\{1,3\}$\|^$\)/d' | perl -nle 'print if m{^[[:ascii:]]+$}' >> /tmp/domain.txt; 
 curl -q -s "http://trustpositif.kominfo.go.id/files/downloads/index.php?dir=database%2Fblacklist%2Fpengaduan%2F&download=domains" | tr '[:upper:]' '[:lower:]' | sed '/\(\/\|:\|=\|\.\.\|\.$\|[0-9]\{1,3\}.[0-9]\{1,3\}.[0-9]\{1,3\}.[0-9]\{1,3\}$\|^$\)/d' | perl -nle 'print if m{^[[:ascii:]]+$}' >> /tmp/domain.txt; 
 dos2unix /tmp/domain.txt;
 
-DATE=$(date +%Y%m%d)
-curr=$(sed -n '/^@/,/^[^;]*)/H;${;x;s/.*@[^(]*([^0-9]*//;s/[^0-9].*//;p;}' $SOAF)
-prefix=${curr::-2}
+###  Check jumlah domain harus lebih besar dari 600 ribu
+if [ $(cat /tmp/domain.txt | wc -l) > 600000 ]; then # 
 if [ "$DATE" -eq "$prefix" ]; then # same day
     num=${curr: -2} # last two digits from serial number
     num=$((10#$num + 1)) # force decimal representation, increment
@@ -38,4 +41,5 @@ echo -e "*.$i\tIN\tA\t103.80.80.246" >> $ZONEF;
 done
 rndc reload
 rndc zonestatus trust.block
+fi
 fi
