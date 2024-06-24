4894c287ceeb6139fd788a58708aa86e85489a20
Syaifuddin JW <ucokkarnadi@users.noreply.github.com>
2024-06-24 22:18:30 +0700

    Update dnsdist.sh

--- dnsdist.sh
+++ dnsdist.sh
@@ -3,6 +3,6 @@ echo "Package: dnsdist*
 Pin: origin repo.powerdns.com
 Pin-Priority: 600
 " > /etc/apt/preferences.d/dnsdist-19
-sudo install -d /etc/apt/keyrings; curl https://repo.powerdns.com/FD380FBB-pub.asc | sudo tee /etc/apt/keyrings/dnsdist-19-pub.asc
-sudo apt update
-sudo apt -y install dnsdist
+install -d /etc/apt/keyrings; curl https://repo.powerdns.com/FD380FBB-pub.asc | tee /etc/apt/keyrings/dnsdist-19-pub.asc
+apt update
+apt -y install dnsdist
