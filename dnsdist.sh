80b531a85d69b823e552041ae66fd9429d7d6f13
Syaifuddin JW <ucokkarnadi@users.noreply.github.com>
2024-06-24 22:07:50 +0700

    Create dnsdist.sh

---
+++ dnsdist.sh
@@ -0,0 +1,8 @@
+echo "deb [signed-by=/etc/apt/keyrings/dnsdist-19-pub.asc] http://repo.powerdns.com/ubuntu noble-dnsdist-19 main" > /etc/apt/sources.list.d/pdns.list
+echo "Package: dnsdist*
+Pin: origin repo.powerdns.com
+Pin-Priority: 600
+" > /etc/apt/preferences.d/dnsdist-19
+sudo install -d /etc/apt/keyrings; curl https://repo.powerdns.com/FD380FBB-pub.asc | sudo tee /etc/apt/keyrings/dnsdist-19-pub.asc
+sudo apt-get update
+sudo apt-get install dnsdist
